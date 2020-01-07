6b3de2a40206f5ef05841469786e04a900ec7193
Aarish Adeel <aarish8@gmail.com>
2020-01-06 20:14:02 -0500

    Add firestore to get and store user data with new routing

--- templates/api.py
+++ templates/api.py
@@ -1,2 +1,36 @@
+import firebase_admin, time, google
+from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1 import ArrayUnion, SERVER_TIMESTAMP
+from flask import Response
+
+cred = credentials.Certificate("westernfood-40032-firebase-adminsdk-pksgj-64f15a1877.json")
+firebase_admin.initialize_app(cred, {'projectId': 'westernfood-40032'})
+
+db = firestore.client()
+
+#TODO: Add authentication
 def get_user_data(user_id):
-    return user_id
+    user_ref = db.collection('users').document(user_id)
+    try:
+        user = user_ref.get()
+        return user.to_dict()
+    except google.cloud.exceptions.NotFound:
+        return Response('failed', 'User does not exist!', 401)
+
+#TODO: Add authentication and error if user does not exist
+def add_user_transaction(transaction, user_id):
+    user_ref = db.collection('users').document(user_id)
+    user_ref.update({'transaction_history': ArrayUnion([transaction])})
+    return Response('Transaction successfully added', 200)
+
+#TODO: Add authentication
+def add_user():
+    user = {
+        'balance': 0,
+        'gross_loaded': 0,
+        'transaction_history': [],
+        'joined_at': SERVER_TIMESTAMP
+    }
+    db.collection('users').add(user)
+
+    return Response('Successfully added user', 200)
--- templates/views.py
+++ templates/views.py
@@ -1,5 +1,21 @@
 from templates import app, api
+from flask import request, Response
 
+#TODO: Add authentication
 @app.route('/api/users/<user_id>', methods=['GET'])
 def get_user_data(user_id):
     return api.get_user_data(user_id)
+
+#TODO: Add authentication
+@app.route('/api/users/<user_id>/transaction_history', methods=['POST'])
+def add_user_transaction(user_id):
+    if not request.content_type == 'application/json':
+        return Response('failed', 'content_type must be application/json', 401)
+
+    transaction = request.get_json()
+    return api.add_user_transaction(transaction, user_id)
+
+#TODO: Add authentication, integrate with user sign up
+@app.route('/api/add_user', methods=['POST'])
+def add_user():
+    return api.add_user()
